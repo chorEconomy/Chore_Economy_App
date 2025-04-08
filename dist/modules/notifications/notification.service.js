34fd4d375f6c65d6f44270f94907ff0a8233344d
Peter Imade <peterosakpolor661@gmail.com>
2025-04-08 21:03:30 +0100

    fix errors

--- dist/modules/notifications/notification.service.js
+++ dist/modules/notifications/notification.service.js
@@ -1,5 +1,5 @@
-import { BadRequestError, NotFoundError } from "../../models/errors";
-import { Notification } from "./notification.model";
+import { BadRequestError, NotFoundError } from "../../models/errors.js";
+import { Notification } from "./notification.model.js";
 class NotificationService {
     static async fetchNotifications(userId) {
         try {
