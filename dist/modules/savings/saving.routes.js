3cf2eb91d97a67bc210e7d69845dc78fb2e7d0cc
Peter Imade <peterosakpolor661@gmail.com>
2025-05-01 03:07:56 +0100

    reoder savings routes

--- dist/modules/savings/saving.routes.js
+++ dist/modules/savings/saving.routes.js
@@ -2,12 +2,14 @@ import authorizeKid from "../../middlewares/authentication/childRoleWare.js";
 import SavingController from "./saving.controller.js";
 import express from "express";
 const savingsRouter = express.Router();
+savingsRouter.get("/savings-reminders", SavingController.TriggerSavingsReminders);
+
 savingsRouter.post("/", authorizeKid, SavingController.CreateSaving);
+savingsRouter.get("/", authorizeKid, SavingController.GetAllSavingsGoals);
+
 savingsRouter.get("/:id", authorizeKid, SavingController.FetchSaving);
 savingsRouter.delete("/:id", authorizeKid, SavingController.DeleteSaving);
 savingsRouter.post("/:id/pay", authorizeKid, SavingController.MakePayment);
 savingsRouter.get("/:id/history", authorizeKid, SavingController.GetSavingsHistory);
-savingsRouter.get("/", authorizeKid, SavingController.GetAllSavingsGoals);
-savingsRouter.get("/savings-reminders", SavingController.TriggerSavingsReminders);
 savingsRouter.patch("/:id/withdraw", authorizeKid, SavingController.WithdrawCompletedSaving);
 export default savingsRouter;
